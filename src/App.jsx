59651297c8c36820deef02b457d8c2a834d74cd5
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-04-28 22:50:54 +0100

    Need help

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,113 @@
-import { useState} from 'react'
+import { useState } from 'react'
 import "./App.css"
 
 function App() {
-  const [calc, setCalc] = useState('')
-  const [result, setResult] = useState('')
 
-  const ops = ['+', '-' , '*', '/' ]
+  const [display, setDisplay] = useState('0') //Diplays value
+  const [firstOperand, setFirstOperand] = useState('') // first Button
+  const [secondOperand,  setSecondOperand]  = useState('') //Second Button
+  const [operator, setOperator] = useState('') //Operator
+  const [result, setResult] = useState('') // Result of Calculation
 
-  const updateCalc = value => {
-    if (
-      ops.includes(value) && calc === '' ||
-      ops.includes(value) && ops.includes (calc.slice(-1))
-    ) 
-    {
-      return
-    }
-    setCalc(calc + value)
-
-    if (!ops.includes(value)) {
-    setResult(eval(calc + value).toString())
+  //button session
+  const handleNumberClick = (num) => {
+    if (operator) {
+      setSecondOperand(secondOperand + num) //ask Lewis 
+      setDisplay(secondOperand + num)
+    } else {
+      setFirstOperand(firstOperand + num)
+      setDisplay(firstOperand + num)
     }
   }
 
+  //operator session
+  const handleOperatorClick = (op) => {
+    if (operator && secondOperand || firstOperand) {
+      calculateResult()
+    }
+    setOperator(op)
+    setDisplay(op)
+  }
+
+  //clear
   const handleClear = () => {
-    setCalc('')
-    setResult ('')
+    setDisplay('0')
+    setFirstOperand('')
+    setSecondOperand('')
+    setOperator('')
+    setResult('')
   }
 
-  const handleEqual = () => {
-    setCalc(eval(calc).toString())
+  const calculateResult = () => {
+    try {
+      const calculate = firstOperand + operator + secondOperand
+      const calculateNow = eval(calculate)
+      setResult(calculateNow.toString())
+      setFirstOperand(calculateNow.toString())
+      setSecondOperand('')
+      setOperator('')
+      setDisplay(calculateNow.toString())
+    } catch (error) {
+      console.log('error evaluating calculate', error)
+    }
   }
+
+  const handleEqualClick = () => {
+    if (firstOperand && secondOperand && operator){
+      calculateResult()
+    }
+  }
+
 
   return (
     <div className="calculator">
       <div className="panel">
-        <p>{ result || 0 }</p>
+        <p>{display || 0}</p>
         <div className="numbers">
-          <button onClick={() => updateCalc('1')}>1</button>
-          <button onClick={() => updateCalc('2')}>2</button>
-          <button onClick={() => updateCalc('3')}>3</button>
-          <button onClick={() => updateCalc('4')}>4</button>
-          <button onClick={() => updateCalc('5')}>5</button>
-          <button onClick={() => updateCalc('6')}> 6</button>
-          <button onClick={() => updateCalc('7')}>7</button>
-          <button onClick={() => updateCalc('8')}>8</button>
-          <button onClick={() => updateCalc('9')}>9</button>
-          <button onClick={() => updateCalc('0')}>0</button>
-          <button onClick={handleClear}>Clear</button>
+          <button onClick={() => handleNumberClick('1')}>1</button>
+          <button onClick={() => handleNumberClick('2')}>2</button>
+          <button onClick={() => handleNumberClick('3')}>3</button>
+          <button onClick={() => handleNumberClick('4')}>4</button>
+          <button onClick={() => handleNumberClick('5')}>5</button>
+          <button onClick={() => handleNumberClick('6')}>6</button>
+          <button onClick={() => handleNumberClick('7')}>7</button>
+          <button onClick={() => handleNumberClick('8')}>8</button>
+          <button onClick={() => handleNumberClick('9')}>9</button>
+          <button onClick={() => handleNumberClick('0')}>0</button>
+          <button onClick={() => handleClear()}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operator}</p>
         <div className="numbers">
-          <button onClick={() => updateCalc('+')}>+</button>
-          <button onClick={() => updateCalc('-')}>-</button>
-          <button onClick={() => updateCalc('*')}>*</button>
-          <button onClick={() => updateCalc('/')}>÷</button>
+          <button onClick={() => handleOperatorClick('+')}>+</button>
+          <button onClick={() => handleOperatorClick('-')}>-</button>
+          <button onClick={() => handleOperatorClick('*')}>*</button>
+          <button onClick={() => handleOperatorClick('/')}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{firstOperand}</p>
         <div className="numbers">
-          <button onClick={() => updateCalc('')}>1</button>
-          <button onClick={() => updateCalc('2')}>2</button>
-          <button onClick={() => updateCalc('3')}>3</button>
-          <button onClick={() => updateCalc('4')}>4</button>
-          <button onClick={() => updateCalc('5')}>5</button>
-          <button onClick={() => updateCalc('6')}>6</button>
-          <button onClick={() => updateCalc('7')}>7</button>
-          <button onClick={() => updateCalc('8')}>8</button>
-          <button onClick={() => updateCalc('9')}>9</button>
-          <button onClick={() => updateCalc('0')}>0</button>
-          <button onClick={handleClear}>Clear</button>
+        <button onClick={() => handleNumberClick('1')}>1</button>
+          <button onClick={() => handleNumberClick('2')}>2</button>
+          <button onClick={() => handleNumberClick('3')}>3</button>
+          <button onClick={() => handleNumberClick('4')}>4</button>
+          <button onClick={() => handleNumberClick('5')}>5</button>
+          <button onClick={() => handleNumberClick('6')}>6</button>
+          <button onClick={() => handleNumberClick('7')}>7</button>
+          <button onClick={() => handleNumberClick('8')}>8</button>
+          <button onClick={() => handleNumberClick('9')}>9</button>
+          <button onClick={() => handleNumberClick('0')}>0</button>
+          <button onClick={() => handleClear()}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{result || 0}</p>
         <div>
-          <button>=</button>
+          <button onClick={() => handleEqualClick()}>=</button>
         </div>
       </div>
     </div>
